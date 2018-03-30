e632e2a97468dd43032b6c2e55b1b7f9d51217c7
mhnHN <37935297+mhnHN@users.noreply.github.com>
2018-03-30 17:46:09 +0200

    Add files via upload

---
+++ WEBKWiese.R
@@ -0,0 +1,5 @@
+#Página web personal Usando como host GITHUB.
+#http://www.emilyzabor.com/tutorials/rmarkdown_websites_tutorial.html
+#K. Wiese Febrero 2018
+#####################################################################
+
